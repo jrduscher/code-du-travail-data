b3457f84733f0edcd4bf2143e1118da526d1dc99
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-09-24 10:24:09 +0200

    fix(fiches-ministere-travail): add html + fix images srcs

--- dataset/fiches_ministere_travail/ministere-travail-extract-fiches.js
+++ dataset/fiches_ministere_travail/ministere-travail-extract-fiches.js
@@ -6,20 +6,29 @@ const FICHES_URLS = require("./ministere-travail-liste-fiches.js");
 
 const qsa = (node, selector) => Array.from(node.querySelectorAll(selector));
 
+// restore base image url
+const fixImages = html =>
+  html.replace(
+    /src="((?!https?:\/\/)[^"]*)"/g,
+    `src="https://travail-emploi.gouv.fr/$1"`
+  );
+
 const parseFiche = url =>
   JSDOM.fromURL(url)
     .then(dom => {
-
       const summary = qsa(dom.window.document, ".navigation-article li")
         .map(n => n.textContent.trim())
         .filter(t => t !== "L’INFO EN PLUS" && t !== "POUR ALLER PLUS LOIN");
 
       // `articles` = textes de référence.
-      const articles = qsa(dom.window.document, "article.encarts__article li")
-        .map(n => ({
-          url: qsa(n, "a") && qsa(n, "a")[0] && qsa(n, "a")[0].getAttribute("href"),
-          text: n.textContent.trim()
-        }));
+      const articles = qsa(
+        dom.window.document,
+        "article.encarts__article li"
+      ).map(n => ({
+        url:
+          qsa(n, "a") && qsa(n, "a")[0] && qsa(n, "a")[0].getAttribute("href"),
+        text: n.textContent.trim()
+      }));
 
       const article = qsa(dom.window.document, ".main-article")[0];
 
@@ -28,20 +37,23 @@ const parseFiche = url =>
         summary,
         title: qsa(article, "h1")[0].textContent.trim(),
         text_full: qsa(article, ".main-article__texte")[0].textContent.trim(),
+        html: fixImages(
+          qsa(article, ".main-article__texte")[0].innerHTML.trim()
+        ),
         text_by_section: [],
-        url,
-      }
+        url
+      };
 
-      const articleChildren = qsa(article, '*');
-      articleChildren.forEach(function (el, index) {
-        if (el.tagName === 'H3') {
+      const articleChildren = qsa(article, "*");
+      articleChildren.forEach(function(el, index) {
+        if (el.tagName === "H3") {
           let subSection = {
             title: el.textContent.trim(),
-            text: '',
-            url: `${url}#${el.id}`,
+            text: "",
+            url: `${url}#${el.id}`
           };
           let nextEl = el.nextElementSibling;
-          while (nextEl && nextEl.tagName !== 'H3') {
+          while (nextEl && nextEl.tagName !== "H3") {
             subSection.text += nextEl.textContent.trim();
             nextEl = nextEl.nextElementSibling;
           }
@@ -50,14 +62,12 @@ const parseFiche = url =>
       });
 
       return result;
-
     })
     .catch(error => {
       // Ignore 404 errors.
+      return null;
     });
 
-serialExec(
-  FICHES_URLS.map(f => () => parseFiche(f.url).catch()).filter(Boolean)
-)
-  .then(j => console.log(JSON.stringify(j, null, 2)))
+serialExec(FICHES_URLS.map(f => () => parseFiche(f.url).catch()))
+  .then(j => console.log(JSON.stringify(j.filter(Boolean), null, 2)))
   .catch(console.log);
