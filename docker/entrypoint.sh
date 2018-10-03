45af271b0a882ad89a67b3d7a2bf2b0253fc6c7c
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-10-03 11:52:22 +0200

    chore: portable docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,10 @@ do
 done
 echo "Elasticsearch instance available: connected successfully."
 
+cd /app
+
+#python ./search/indexing/create_indexes.py;
+
 # Infinite wait: keep the container alive until it is told to stop.
 # Using trap will make the container react immediately to a stop request.
 /bin/sh -c "trap : TERM INT; while sleep 3600; do :; done"
